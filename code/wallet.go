a96af003c9d400bfc21de2edb5148943ad38709a
Bar11 <zxg259370@163.com>
2025-02-28 23:23:54 +0800

    钱包与钱包文件管理
    Signed-off-by: Bar11 <zxg259370@163.com>

--- code/wallet.go
+++ code/wallet.go
@@ -1,1 +1,80 @@
 package code
+
+import (
+	"bytes"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"log"
+)
+
+const version = byte(0x00)
+const walletfile = "wallet.dat"
+
+// 检测地址长度
+const addressChecksumLen = 4
+
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+// 创建一个钱包
+func NewWallet() *Wallet {
+	privatekey, publickey := newKeyPair()
+	wallet := Wallet{privatekey, publickey}
+	return &wallet
+}
+
+// 生成公钥私钥
+func newKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+	// 生成密钥
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+	publickey := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
+	return *private, publickey
+}
+
+// 公钥的校验
+func checkSum(data []byte) []byte {
+	firstSHA := sha256.Sum256(data) // 加密校验
+	secondSHA := sha256.Sum256(firstSHA[:])
+	return secondSHA[:addressChecksumLen]
+}
+
+// 公钥的哈希处理
+func HashPubKey(pubkey []byte) []byte {
+	publicsha256 := sha256.Sum256(pubkey)     // 处理公钥
+	R160Hash := crypto.RIPEMD160.New()        // 创建一个哈希算法对象
+	_, err := R160Hash.Write(publicsha256[:]) // 写入处理
+	if err != nil {
+		log.Panic(err)
+	}
+	publicR160Hash := R160Hash.Sum(nil) // 叠加运算
+	return publicR160Hash
+}
+
+// 获取钱包地址
+func (w *Wallet) GetAddress() []byte {
+	pubKeyHash := HashPubKey(w.PublicKey)
+	versionedPayload := append([]byte{version}, pubKeyHash...)
+	checksum := checkSum(versionedPayload) // 检测版本与公钥
+	fullPayload := append(versionedPayload, checksum...)
+	address := Base58Encode(fullPayload)
+	return address
+}
+
+// 校验钱包地址
+func ValidateAddress(address string) bool {
+	publicHash := Base58Decode([]byte(address))
+	actualChecksum := publicHash[len(publicHash)-addressChecksumLen:]
+	version := publicHash[0]
+	publicHash = publicHash[1 : len(publicHash)-addressChecksumLen]
+	targetCheckSum := checkSum(append([]byte{version}, publicHash...))
+	return bytes.Compare(actualChecksum, targetCheckSum) == 0
+}
--- code/wallets.go
+++ code/wallets.go
@@ -1,1 +1,85 @@
 package code
+
+import (
+	"bytes"
+	"crypto/elliptic"
+	"encoding/gob"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+const walletFile = "wallet_%s.dat"
+
+type Wallets struct {
+	Wallets map[string]*Wallet
+}
+
+// 新建一个钱包，或者获取已存在的钱包
+func NewWallets(nodeID string) (*Wallets, error) {
+	wallets := Wallets{}
+	wallets.Wallets = make(map[string]*Wallet)
+	err := wallets.LoadFromFile(nodeID)
+	return &wallets, err
+}
+
+func (wallets *Wallets) CreateWallet() string {
+	wallet := NewWallet()
+	address := fmt.Sprintf("%s", wallet.GetAddress())
+	wallets.Wallets[address] = wallet
+	return address
+}
+
+// 获取所有钱包地址
+func (wallets *Wallets) GetAddresses() []string {
+	var addresses []string
+	for address := range wallets.Wallets {
+		addresses = append(addresses, address)
+	}
+	return addresses
+}
+
+// 获取一个钱包
+func (wallets *Wallets) GetWallte(address string) Wallet {
+	return *wallets.Wallets[address]
+}
+
+// 从文件中读取钱包
+func (wallets *Wallets) LoadFromFile(nodeID string) error {
+
+	mywalletfile := fmt.Sprintf(walletFile, nodeID)
+	if _, err := os.Stat(mywalletfile); os.IsNotExist(err) {
+		return err
+	}
+	fileContent, err := ioutil.ReadFile(mywalletfile)
+	if err != nil {
+		log.Panic(err)
+	}
+	// 读取二进制文件并解析
+	var wallets_ Wallets
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
+	err = decoder.Decode(&wallets) //解码
+	if err != nil {
+		log.Panic(err)
+	}
+	wallets.Wallets = wallets_.Wallets
+	return nil
+}
+
+// 钱包保存到文件
+func (wallets *Wallets) SaveToFile(nodeID string) {
+	var content bytes.Buffer
+	mywalletfile := fmt.Sprintf(walletFile, nodeID)
+	gob.Register(elliptic.P256()) // 注册加密算法
+	encoder := gob.NewEncoder(&content)
+	err := encoder.Encode(wallets)
+	if err != nil {
+		log.Panic(err)
+	}
+	err = ioutil.WriteFile(mywalletfile, content.Bytes(), 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+}
